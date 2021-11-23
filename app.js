490dfc16b8ef052359dcc7349d7c20e35a714a0c
mkhoa0201 <mkhoa0201@gmail.com>
2021-11-23 15:31:25 +0200

    add list.js and date.js

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
+let items = [];
+let workItems = []; 
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  var today = new Date();
-  var currentDay = today.getDay();
+  
+  let day = date();
 
-  switch (currentDay) {
-    case 0 :
-      day = "Sunday";
-      break;
-    case 1 :
-      day = "Monday";
-      break;
-    case 2 :
-      day = "Tuesday";
-      break;
-    case 3 :
-      day = "Wednesday";
-      break;
-    case 4 :
-      day = "Thursday";
-      break;
-    case 5 :
-      day = "Friday";
-      break;
-    case 6 :
-      day = "Saturday";
-      break;
-
-    default:
-      console.log("Error!!!" + currentDay);
-
-  }
-  res.render("list", {kindOfDay: day});
+    res.render("list", {listTitle: day, newListItems: items});
+  
 
 });
+
+// handle the post request
+app.post("/", function(req,res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+    console.log(req.body);
+  } else {
+    items.push(item);
+    res.redirect("/");
+    console.log(req.body);
+  
+  }
+});
+
+app.get("/work", function(req,res) {
+  res.render("list",{listTitle:"Work", newListItems: workItems});
+});
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
---
+++ date.js
@@ -0,0 +1,16 @@
+
+module.exports = getDate = function() {
+    const today = new Date();
+  
+    const options = {
+     weekday: "long",
+     day: "numeric",
+     month: "long",
+
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
+
